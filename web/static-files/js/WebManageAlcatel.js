7364fba1eba1bacf052b447bbf107799a2c5db94
Nicolas Montes <nicolas.montes.perso@gmail.com>
2015-01-11 21:11:46 +0100

    Add Javascript

---
+++ web/static-files/js/WebManageAlcatel.js
@@ -0,0 +1,9 @@
+function sendDelete() {
+	var id = document.getElementById('id');
+	console.log("/DSLAM?id=" + id.value);
+	var url = "/DSLAM?id=" + id.value;
+	xmlhttp= new XMLHttpRequest();
+	xmlhttp.open("DELETE", url, true);
+	xmlhttp.send();
+	//document.location.href="/option";
+}
